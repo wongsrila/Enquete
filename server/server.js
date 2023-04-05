455bf7a964de1d4152f4959369064d7953a31963
wongsrila <jiraphong.wongsrila@gmail.com>
2023-04-06 00:15:51 +0200

    added server API

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/send', (req, res) => {
+  // Deze gegevens sla je vervolgens op in de database met MongoDB/Mongoose
+  console.log(req.body);
+
+  // vervolgens stuur je een reactie terug naar de client dat het successvol is verstuurd.
+  res.send('Je enquete is successvol vezonden!');
+});
+
+app.listen(port, () =>
+  console.log(`Listening on port http://localhost:${port}/`),
+);
